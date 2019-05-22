70cd034af5df9d805d19a492fe1567b4749c7759
n-b-sonar <nicolas.bontoux@sonarsource.com>
2019-05-22 13:31:21 +0200

    Bootstrap

---
+++ colors.js
@@ -0,0 +1,20 @@
+/* Neutral */
+
+export const baseBackground = '#fff';
+export const black100 = '#fff'; // rename me!
+export const lightBackgroundHighlight = '#f9f9fb';
+export const black300 = '#cfd3d7';
+export const black400 = '#a0a5a8';
+export const black500 = '#8a8c8f';
+export const black600 = '#65666a';
+export const black700 = '#434447';
+export const black800 = '#2d3032';
+export const black900 = '#070706';
+
+/* Accent - red (sonarsource) */
+export const red200 = '#ff797e';
+export const red300 = '#e93d44';
+export const red500 = '#c9232c';
+export const red600 = '#a71918';
+export const red700 = '#8d1b19';
+export const red700 = '#8d1b19';
